1ddd6c42b342097ad4b292b3b52ead4f7cb14e7a
Theodore Guillot <dexter.gamer@laposte.net>
2011-11-06 20:26:23 +0100

    Implemented PowerImplementation2 in PowerImplementation.cs and declared Vector3D.

--- src/Mooege/Core/GS/Powers/PowerImplementation.cs
+++ src/Mooege/Core/GS/Powers/PowerImplementation.cs
@@ -16,13 +16,54 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
+using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
 using Mooege.Common;
+using Mooege.Core.GS.Actors;
+using Mooege.Net.GS.Message.Definitions.World;
+using Mooege.Net.GS.Message.Fields;
+using Mooege.Common.Helpers;
 using Mooege.Core.GS.Ticker.Helpers;
+using Mooege.Core.GS.Common.Types.Math;
 
 namespace Mooege.Core.GS.Powers
 {
-    public abstract class PowerImplementation : PowerContextHelper
+    public abstract class PowerImplementation2
+    {
+        private static Dictionary<int, Type> _implementations = new Dictionary<int, Type>();
+
+        public static PowerImplementation2 ImplementationForPowerSNO(int powerSNO)
+        {
+            if (_implementations.ContainsKey(powerSNO))
+            {
+                return (PowerImplementation2)Activator.CreateInstance(_implementations[powerSNO]);
+            }
+
+            return null;
+        }
+
+        static PowerImplementation2()
+        {
+            foreach (Type type in Assembly.GetExecutingAssembly().GetTypes())
+            {
+                if (type.IsSubclassOf(typeof(PowerImplementation2)))
+                {
+                    var attributes = (ImplementsPowerSNO[])type.GetCustomAttributes(typeof(ImplementsPowerSNO), true);
+                    foreach (var powerAttribute in attributes)
+                    {
+                        _implementations[powerAttribute.PowerSNO] = type;
+                    }
+                }
+            }
+        }
+
+        public abstract IEnumerable<int> Run(Actor player, Actor target, Vector3D mousePosition, TargetMessage msg);
+    }
+	
+	public abstract class PowerImplementation : PowerContextHelper
     {
         public static readonly Logger Logger = LogManager.CreateLogger();
 
