0a515b35bc210a3972342e20ec7c77098a96956c
Theodore Guillot <dexter.gamer@laposte.net>
2011-11-09 21:31:15 +0100

    Implemented 2 DH skills and updated environment

--- src/Mooege/Core/GS/Common/Types/Math/Vector3D.cs
+++ src/Mooege/Core/GS/Common/Types/Math/Vector3D.cs
@@ -179,5 +179,10 @@ namespace Mooege.Core.GS.Common.Types.Math
         {
             return string.Format("x:{0} y:{1} z:{2}",X, Y,Z);
         }
+
+        internal Vector3D RotateOnZ(int p)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
--- src/Mooege/Core/GS/Powers/Implementations/DemonHunter.cs
+++ src/Mooege/Core/GS/Powers/Implementations/DemonHunter.cs
@@ -37,10 +37,11 @@ using Mooege.Net.GS.Message.Definitions.ACD;
 using Mooege.Net.GS.Message.Definitions.Animation;
 using Mooege.Net.GS.Message.Definitions.Player;
 using Mooege.Core.GS.Common.Types.Math;
+using Mooege.Core.GS.Map;
 
 namespace Mooege.Core.GS.Powers.Implementations
 {
-	[ImplementsPowerSNO(Skills.Skills.DemonHunter.HatredGenerators.HungeringArrow)]
+    [ImplementsPowerSNO(Skills.Skills.DemonHunter.HatredGenerators.HungeringArrow)]
     public class DemonHunterHungeringArrow : PowerImplementation2
     {
         public override IEnumerable<int> Run(Actor owner, Actor target, Vector3D mousePosition, TargetMessage msg)
@@ -89,6 +90,417 @@ namespace Mooege.Core.GS.Powers.Implementations
 
                 yield return 100;
             }
+        }
+    }
+
+    [ImplementsPowerSNO(Skills.Skills.DemonHunter.HatredGenerators.BolaShot)]
+    public class DemonHunterBolaShot : PowerImplementation2
+    {
+        public override IEnumerable<int> Run(Actor owner, Actor target, Vector3D mousePosition, TargetMessage msg)
+        {
+            //Cast owner to player
+            Mooege.Core.GS.Players.Player hero = owner as Mooege.Core.GS.Players.Player;
+
+            //Show cast effect on player
+            hero.Effect2.addEffect2(176854);
+
+            //Regen hatred
+            hero.GeneratePrimaryResource(5f);
+
+            //Spawn Projectile actor
+            PowerProjectile powerProjectile = new PowerProjectile(hero.World, 77569, hero.Position, mousePosition, 1f, 3000, 0.5f, 1f, 6f, 3f);
+
+            //Every 100ms calculate if an impact occure with the projectile and check if the projectile is still alive
+            while (powerProjectile.World != null)
+            {
+                //Check if projectile enter in collision with a monster
+                if (powerProjectile.detectCollision())
+                {
+                    //Destroy projectile
+                    powerProjectile.Destroy();
+
+                    //Play bola constriction effect
+                    powerProjectile.hittedActor.Effect2.addEffect2(153874);
+
+                    //Wait 1000 sec b4 explosion
+                    yield return 1000;
+
+                    //Play explosion effect 
+                    powerProjectile.hittedActor.Effect2.addEffect2(153727);
+
+                    //Deal dmg to main target
+                    powerProjectile.hittedActor.ReceiveDamage(50f, FloatingNumberMessage.FloatType.White);
+
+                    //Apply aoe effect
+                    /*foreach (Actor actor in powerProjectile.hittedActor.World.GetActorsInRange(powerProjectile.hittedActor.Position, 15f))
+                    {
+                        if (actor.ActorType == ActorType.Monster && actor.DynamicID != powerProjectile.hittedActor.DynamicID)
+                            actor.ReceiveDamage(30f, FloatingNumberMessage.FloatType.White);
+                    }*/
+
+                }
+
+                yield return 100;
+            }
+        }
+    }
+
+    [ImplementsPowerSNO(Skills.Skills.DemonHunter.HatredGenerators.EntanglingShot)]
+    public class DemonHunterEntanglingShot : PowerImplementation2
+    {
+        public override IEnumerable<int> Run(Actor owner, Actor target, Vector3D mousePosition, TargetMessage msg)
+        {
+            //Cast owner to player
+            Mooege.Core.GS.Players.Player hero = owner as Mooege.Core.GS.Players.Player;
+
+            //Regen hatred
+            hero.GeneratePrimaryResource(5f);
+
+            //Spawn Projectile actor
+            PowerProjectile powerProjectile = new PowerProjectile(hero.World, 75678, hero.Position, mousePosition, 1f, 3000, 1f, 1f, 6f, 3f);
+
+            //Init list of actor affected by snare
+            List<Actor> snaredActor = new List<Actor>();
+
+            //Every 100ms calculate if an impact occure with the projectile and check if the projectile is still alive
+            while (powerProjectile.World != null)
+            {
+                //Check if projectile enter in collision with a monster
+                if (powerProjectile.detectCollision())
+                {
+                    //Destroy projectile
+                    powerProjectile.Destroy();
+
+                    //Play hit effect
+                    powerProjectile.hittedActor.Effect2.addEffect2(76223);
+
+                    //Receive dmg / Snared
+                    powerProjectile.hittedActor.ReceiveDamage(50f, FloatingNumberMessage.FloatType.White);
+                    powerProjectile.hittedActor.ReceiveDamage(0f, FloatingNumberMessage.FloatType.Snared);
+
+                    //Reduce movement speed of monster to 0
+                    //TODO
+
+                    //Check if we can chain effect
+                    /*Actor chainActor = powerProjectile.hittedActor.World.GetActorsInRange(powerProjectile.hittedActor.Position, 15f).FirstOrDefault(a => a.ActorType == ActorType.Monster);
+
+                    //We got a chained actor  //76093
+                    if (chainActor != null)
+                    {
+                        chainActor.ReceiveDamage(0f, FloatingNumberMessage.FloatType.Snared);
+                        chainActor.Effect2.addEffect2(76227);
+
+                        //Reduce movement speed of monster to 0
+                        //TODO
+                    }
+
+                    //Add snared target to snared list                    
+                    snaredActor.Add(powerProjectile.hittedActor);
+                    snaredActor.Add(chainActor);
+
+                }*/
+
+                    yield return 100;
+                }
+
+                yield return 2000;
+
+                foreach (Actor actor in snaredActor)
+                {
+                    //Put back normal movement speed
+                    //TODO
+                }
+            }
+        }
+    }
+	
+	/*[ImplementsPowerSNO(Skills.Skills.DemonHunter.HatredGenerators.EvasiveFire)]
+    public class DemonHunterEvasiveFire : PowerImplementation2
+    {
+        public override IEnumerable<int> Run(Actor owner, Actor target, Vector3D mousePosition, TargetMessage msg)
+        {
+            //Cast owner to player
+            Mooege.Core.GS.Players.Player hero = owner as Mooege.Core.GS.Players.Player;
+
+            //Regen hatred
+            hero.GeneratePrimaryResource(10f);
+
+            hero.Effect2.addEffect2(150649);
+
+            if (target != null)
+            {
+                target.Effect2.addEffect2(150799);
+                target.ReceiveDamage(50f, FloatingNumberMessage.FloatType.White);
+
+                Actor closeActor = hero.World.GetActorsInRange(hero.Position, 15f).FirstOrDefault(a => a.ActorType == ActorType.Monster);
+
+                //Close actor found, backflip
+                if (closeActor != null)
+                {
+                    //Use discipline
+                    hero.UseSecondaryResource(4f);
+
+                    //Get normal inverted vector
+                    Vector3D invertedNormVector = PowerUtils.invertVector(PowerUtils.getNormalVector(target.Position, hero.Position));
+
+                    Console.Write(invertedNormVector.X + ":" + invertedNormVector.Y);
+
+                    //Calculate final translate point
+                    Vector3D finalPosition = new Vector3D(hero.Position.X + invertedNormVector.X * 15f, hero.Position.Y + invertedNormVector.Y * 15f, hero.Position.Z);
+
+                    //Translate backward
+                    hero.Movement.translateNormal(finalPosition, 69792);
+                }
+            }
+
+            yield break;
+        }
+    }*/
+	
+	/*[ImplementsPowerSNO(Skills.Skills.DemonHunter.HatredGenerators.Grenades)]
+    public class DemonHunterGrenades : PowerImplementation2
+    {
+        public override IEnumerable<int> Run(Actor owner, Actor target, Vector3D mousePosition, TargetMessage msg)
+        {
+            //Cast owner to player
+            Mooege.Core.GS.Players.Player hero = owner as Mooege.Core.GS.Players.Player;
+
+            Vector3D diffVector = new Vector3D(mousePosition.X - hero.Position.X, mousePosition.Y - hero.Position.Y, mousePosition.Z - hero.Position.Z);
+            Dictionary<PowerProjectile, Vector3D> grenadeList = new Dictionary<PowerProjectile, Vector3D>();
+
+            //Create 3 grenate projectile
+            for (int i = -1; i < 2; i++)
+            {
+                PowerProjectile projectile = new PowerProjectile(hero.World, 88244, hero.Position, mousePosition, 1f, 3000, 1f, 1f, 6f, 3f, true);
+
+                //Deviate grenade slightly from each other
+                Vector3D deviateDiff = diffVector.RotateOnZ(i * RandomHelper.Next(10, 20));
+
+                //FIXME : this prob use translate deth path Sin
+                //Calculate final posistion
+                Vector3D arcFinalPos = new Vector3D(hero.Position.X + (deviateDiff.X), hero.Position.Y + (deviateDiff.Y), mousePosition.Z);
+                grenadeList[projectile] = arcFinalPos;
+
+                //Calculate first leap final posistion
+                Vector3D firstArcFinalPos = new Vector3D(hero.Position.X + (deviateDiff.X * 0.8f), hero.Position.Y + (deviateDiff.Y * 0.8f), mousePosition.Z);
+
+                //Translate grenade
+                projectile.Movement.translateArc(firstArcFinalPos, -1, 1.1f);
+            }
+
+            //First arc
+            yield return 420;
+
+            foreach (KeyValuePair<PowerProjectile, Vector3D> grenade in grenadeList)
+            {
+                grenade.Key.Movement.translateArc(grenade.Value, -1, 0.8f);
+            }
+
+            yield return 500;
+
+            foreach (KeyValuePair<PowerProjectile, Vector3D> grenade in grenadeList)
+            {
+                grenade.Key.Effect2.addEffect2(153982);
+                foreach (Actor actor in grenade.Key.World.GetActorsInRange(grenade.Key.Position, 5f))
+                {
+                    if (actor.ActorType == ActorType.Monster)
+                    {
+                        actor.ReceiveDamage(10f, FloatingNumberMessage.FloatType.White);
+                    }
+                }
+            }
+
+            yield return 200;
+
+            foreach (KeyValuePair<PowerProjectile, Vector3D> grenade in grenadeList)
+            {
+                grenade.Key.Destroy();
+            }
+
+            yield break;
+        }
+    }*/
+
+    [ImplementsPowerSNO(Skills.Skills.DemonHunter.HatredSpenders.Impale)]
+    public class DemonHunterImpale : PowerImplementation2
+    {
+        public override IEnumerable<int> Run(Actor owner, Actor target, Vector3D mousePosition, TargetMessage msg)
+        {
+            //Cast owner to player
+            Mooege.Core.GS.Players.Player hero = owner as Mooege.Core.GS.Players.Player;
+
+            yield return 100;
+
+            //Regen hatred
+            hero.UsePrimaryResource(40f);
+
+            hero.Effect2.addEffect2(150233);
+
+            if (target != null)
+            {
+                target.Effect2.addEffect2(180667);
+                target.ReceiveDamage(50f, FloatingNumberMessage.FloatType.White);
+            }
+
+            yield break;        
+        }
+    }
+
+    /*[ImplementsPowerSNO(Skills.Skills.DemonHunter.HatredSpenders.FanOfKnives)]
+    public class DemonHunterFanOfKinves : PowerImplementation2
+    {
+        public override IEnumerable<int> Run(Actor owner, Actor target, Vector3D mousePosition, TargetMessage msg)
+        {
+            //Cast owner to player
+            Mooege.Core.GS.Players.Player hero = owner as Mooege.Core.GS.Players.Player;
+
+            //Regen hatred
+            hero.UsePrimaryResource(20f);
+
+            //Cast effect
+            hero.Effect2.addEffect2(77547);
+
+            foreach(Actor actor in hero.World.GetActorsInRange(hero.Position, 10f))
+            {
+                if (actor.ActorType == ActorType.Monster)
+                {
+                    actor.ReceiveDamage(50f, FloatingNumberMessage.FloatType.White);
+                    actor.ReceiveDamage(0f, FloatingNumberMessage.FloatType.AttackSlowed);
+                }
+            }
+
+            yield break;
+        }
+    }*/
+
+    /*[ImplementsPowerSNO(Skills.Skills.DemonHunter.HatredSpenders.Chakram)]
+    public class DemonHunterChakram : PowerImplementation2
+    {
+        public override IEnumerable<int> Run(Actor owner, Actor target, Vector3D mousePosition, TargetMessage msg)
+        {
+            //Cast owner to player
+            Mooege.Core.GS.Players.Player hero = owner as Mooege.Core.GS.Players.Player;
+
+            //Regen hatred
+            hero.UsePrimaryResource(20f);
+
+            PowerProjectile projectile = new PowerProjectile(hero.World, 129228, hero.Position, mousePosition, 1f, 3000, 1f, 1f, 0f, 3f, true);
+            projectile.Movement.translateSpiral(mousePosition);
+            
+
+            yield break;
+        }
+    }*/
+
+    /*[ImplementsPowerSNO(Skills.Skills.DemonHunter.Discipline.Caltrops)]
+    public class DemonHunterCaltrops : PowerImplementation2
+    {
+        public override IEnumerable<int> Run(Actor owner, Actor target, Vector3D mousePosition, TargetMessage msg)
+        {
+            //Cast owner to player
+            Mooege.Core.GS.Players.Player hero = owner as Mooege.Core.GS.Players.Player;
+
+            //Regen hatred
+            //hero.useSecondaryResources(8f);
+
+            //Cast effect
+            hero.Effect2.addEffect2(131623);
+
+            //Spawn the catual proxy trap
+            PowerProxy trap = new PowerProxy(hero.World, 136149, hero.Position);
+            trap.Effect.addEffect(196032);
+
+            //Calculate trap ttl
+            int trapTtl = hero.World.Game.Tick + (60 * 12);
+
+            //List of monster that have already been trapped, monster can only be trapped once by a trap
+            List<Actor> trappedTarget = new List<Actor>();
+
+            //Watch for monster proximity for the next 12 sec
+            while (hero.World.Game.Tick < trapTtl)
+            {
+                foreach (Actor actor in trap.World.GetActorsInRange(trap.Position, 10f))
+                {
+                    if (actor.ActorType == ActorType.Monster && !trappedTarget.Contains(actor))
+                    {
+                        actor.Effect.addEffect(131623);
+                        actor.ReceiveDamage(50f, FloatingNumberMessage.FloatType.White);
+                        actor.ReceiveDamage(0f, FloatingNumberMessage.FloatType.AttackSlowed);
+                        trappedTarget.Add(actor);
+                    }
+                }
+
+                yield return 100;
+            }
+
+            //Destroy trap after 12 sec
+            trap.Destroy();
+
+            yield break;
+        }
+    }*/
+
+    /*[ImplementsPowerSNO(Skills.Skills.DemonHunter.Discipline.Vault)]
+    public class DemonHunterVault : PowerImplementation2
+    {
+        public override IEnumerable<int> Run(Actor owner, Actor target, Vector3D mousePosition)
+        {
+            //Cast owner to player
+            Mooege.Core.GS.Players.Player hero = owner as Mooege.Core.GS.Players.Player;
+
+            //Regen hatred
+            hero.UseSecondaryResource(8f);
+
+            //hero.ActorAttribute.setAttribute(GameAttribute.Power_Buff_0_Visual_Effect_None, new GameAttributeValue(true), Skills.Skills.DemonHunter.Discipline.Vault);
+
+            //Translate backward
+            hero.Movement.translateNormal(mousePosition, 69792, 2f, PowerUtils.getRadian(hero.Position, mousePosition));
+
+            yield break;
+        }
+    }*/
+	
+	/*[ImplementsPowerSNO(Skills.Skills.DemonHunter.Discipline.MarkedForDeath)]
+    public class DemonHunterMarkedForDeath : PowerImplementation2
+    {
+        public override IEnumerable<int> Run(Actor owner, Actor target, Vector3D mousePosition, TargetMessage msg)
+        {
+            //Cast owner to player
+            Mooege.Core.GS.Players.Player hero = owner as Mooege.Core.GS.Players.Player;
+
+            if (target == null) { yield break; }
+
+            //Regen hatred
+            //hero.useSecondaryResources(8f);
+
+            target.ActorAttribute.setAttribute(GameAttribute.Power_Buff_0_Visual_Effect_None, new GameAttributeValue(true), Skills.Skills.DemonHunter.Discipline.MarkedForDeath);
+            //Add dmg buff
+
+            yield return 30000;
+
+            target.ActorAttribute.setAttribute(GameAttribute.Power_Buff_0_Visual_Effect_None, new GameAttributeValue(false), Skills.Skills.DemonHunter.Discipline.MarkedForDeath);
+            //Remove dmg buff
+        }
+    }*/
+
+    [ImplementsPowerSNO(Skills.Skills.DemonHunter.Discipline.SmokeScreen)]
+    public class DemonHunterSmokeScreen : PowerImplementation2
+    {
+        public override IEnumerable<int> Run(Actor owner, Actor target, Vector3D mousePosition, TargetMessage msg)
+        {
+            //Cast owner to player
+            Mooege.Core.GS.Players.Player hero = owner as Mooege.Core.GS.Players.Player;
+
+            //Regen hatred
+            //hero.useSecondaryResources(8f);
+
+            hero.Effect2.addEffect2(131425);
+            //Add untargetable attribute
+
+            yield return 2000;
+
+            hero.Effect2.addEffect2(133698);
+            //Remove untargetable attribute
         }
     }
 }
